af4cffb7852e3c13779af9765899086923e63850
Gumaro Melendez <cloudernew@gmail.com>
2011-10-19 23:31:37 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pushbroom::Application.config.secret_token = '95747c54477e5edd6acfb2670a7e5cb259786762a83d42a88311d3ddbf152af7adcd9c130bb9fe4858152367a2661687e7f4f2ed497c77228f32a6197f8d6114'
