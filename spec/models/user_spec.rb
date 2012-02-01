9b35e9f12e2f3002bbcbee1f50cb4876e93e5ef4
Gumaro Melendez <cloudernew@gmail.com>
2012-02-01 11:34:53 -0700

    simplified user_spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,53 +1,39 @@
 require 'spec_helper'
 
 describe User do
-	before :each do
-		@user = User.new
-		@attrs = { :email => 'push@broom.com', :token => 'token', :secret => 'secret' }
-	end
+  context 'with missing email, token, or secret' do
+    before :each do
+      @user = User.new
+      @user.valid?
+    end
 
-  it 'should not be valid when missing all attributes' do
-  	@user.should_not be_valid, 'User was valid missing all attributes'
+    it 'is invalid' do
+      @user.should_not be_valid
+    end
+
+    it 'has an error on email' do
+      @user.errors.get(:email).should be
+    end
+
+    it 'has an error on token' do
+      @user.errors.get(:token).should be
+    end
+
+    it 'has an error on secret' do
+      @user.errors.get(:secret).should be
+    end
   end
 
-  it 'should not be valid with only email' do
-  	@user.email = 'push@broom.com'
-  	@user.should_not be_valid, 'User was valid with nothing more than an email'
+  context 'with email, token, and secret' do
+    before :each do
+      @user = User.new :email => 'push@broom.com', :token => 'token', :secret => 'secret'
+    end
+    it 'is valid' do
+      @user.should be_valid
+    end
   end
 
-  it 'should not be valid with only a token' do
-  	@user.token = 'token'
-  	@user.should_not be_valid, 'User was valid with nothing more than a token'
-  end
-
-  it 'should not be valid with only a secret' do
-  	@user.secret = 'secret'
-  	@user.should_not be_valid, 'User was valid with nothing more than a secret'
-  end
-
-  it 'should not be valid when missing an email' do
-  	@user.attributes = @attrs
-  	@user.email = nil
-  	@user.should_not be_valid, 'User was valid without an email'
-  end
-
-  it 'should not be valid when token is missing' do
-  	@user.attributes = @attrs
-  	@user.token = nil
-  	@user.should_not be_valid, 'User was valid without a token'
-  end
-
-  it 'should not be valid when secret is missing' do
-  	@user.attributes
-  	@user.secret = nil
-  	@user.should_not be_valid, 'User was valid without a secret'
-  end
-
-  it 'should be valid when no attributes are missing' do
-  	User.new(@attrs).should be_valid
-  end
-
-  it 'should have many brooms' do
-  	@user.should respond_to(:brooms)
+  it 'has many brooms' do
+    User.new.should respond_to :brooms
   end
 end
